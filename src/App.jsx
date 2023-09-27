2513c08e8921ce736e5f94341281384145f733d5
JeanLamego <jeanlamego@hotmail.com>
2023-09-27 19:04:00 -0300

    prettier added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import Footer from "./Components/Footer"
-import Header from "./Components/Header"
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
-  
-  return (
-    <div >
-      <div className="w-[1500px] h-[1379px] bg-[#252F3D]">
-        <Header/>
-        <Footer/>
-      </div>
-
-    </div>
-      
-    
-  )
+    return (
+        <div>
+            <div className="w-[1500px] h-[1379px] bg-[#252F3D]">
+                <Header />
+                <Banner />
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
