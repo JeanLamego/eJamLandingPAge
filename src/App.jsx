f1945a3526a613bc38539e8ccb52331c993f885a
JeanLamego <jeanlamego@hotmail.com>
2023-09-27 19:55:33 -0300

    configuring tailwind for responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './Components/Header';
 function App() {
     return (
         <div>
-            <div className="w-[1500px] h-[1379px] bg-[#252F3D]">
+            <div className="w-full lg:w-[1500px] lg:h-[1379px] bg-[#252F3D]">
                 <Header />
                 <div className="bg-white">
                     <Banner />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    screens: {
+      'sm': '375px',
+      'inter': '540px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px'
+    },
     extend: {},
   },
   plugins: [],
