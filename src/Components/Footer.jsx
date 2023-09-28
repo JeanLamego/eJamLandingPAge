a3d93df3c0f413d13cd6190497667aedacc93539
JeanLamego <jeanlamego@hotmail.com>
2023-09-28 19:11:22 -0300

    feat:footer complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <div className="w-full px-32 py-6 text-white">
-        <div className="flex justify-between">
-          <p>Copyright (C) 2023 | clarifionsupport@clarifion.com</p>
-          <div className="flex gap-4">
+      <div className="w-full sm:p-5  lg:px-32 sm:py-6 text-white">
+        <div className="flex lg:flex-row sm:flex-col sm:text-xs lg:text-base sm:gap-4 sm:justify-center sm:items-center lg:justify-between">
+          <p>Copyright (C) 2023<span className="text-sm p-2">|</span>clarifionsupport@clarifion.com</p>
+          <div className="flex capitalize gap-4">
             <img
               src="lockIcon.svg
             "
