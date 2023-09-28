0a2ba36b3362f666f23d5131658357f86471cf1b
JeanLamego <jeanlamego@hotmail.com>
2023-09-28 19:23:33 -0300

    feat:banner responsivity done

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,27 +3,59 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div className='sm:px-5 sm:pt-12 lg:px-32'>
-                <div className='flex flex-col sm:text-center  capitalize sm:gap-6 items-center justify-center'>
-                    <h2 className='font-normal sm:text-3xl lg:text-5xl'>Wait ! your order in progress.</h2>
-                    <h1 className='sm:text-base lg:text-2xl text-[#4D5254]'>Lorem ipsum dolor sit amet, consectetur adipiscing </h1>
+            <div className="sm:px-5 sm:pt-12 lg:px-32">
+                <div className="flex flex-col sm:text-center  capitalize sm:gap-6 items-center justify-center">
+                    <h2 className="font-normal sm:text-3xl lg:text-5xl">
+                        Wait ! your order in progress.
+                    </h2>
+                    <h1 className="sm:text-base lg:text-2xl text-[#4D5254]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing{' '}
+                    </h1>
                 </div>
-                <div className='flex  pb-10 pt-14 justify-between'>
-                    <div className='flex items-center gap-5 text-xl'>
-                        <img className="w-10" src="tipCircleIcon.svg" alt="" />
-                        <p >Step 1 : Cart Review</p>
+
+                <div className="flex lg:pb-10 lg:pt-14 justify-between">
+                    <div className="flex sm:flex-col items-center sm:gap-2 sm:py-6  lg:gap-5 sm:text-xs lg:text-xl">
+                        <img
+                            className="sm:w-5  lg:w-10"
+                            src="tipCircleIcon.svg"
+                            alt=""
+                        />
+                        <p>
+                            <span className="sm:hidden">Step 1 : </span>Cart
+                            Review
+                        </p>
                     </div>
-                    <div className='flex items-center gap-5 text-xl'>
-                        <img className="w-10" src="tipCircleIcon.svg" alt="" />
-                        <p >Step 2 : Checkout</p>
+                    <div className="flex sm:flex-col items-center sm:gap-2 sm:py-6  lg:gap-5 sm:text-xs lg:text-xl">
+                        <img
+                            className="sm:w-5  lg:w-10"
+                            src="tipCircleIcon.svg"
+                            alt=""
+                        />
+                        <p>
+                            <span className="sm:hidden">Step 2 :</span>Checkout
+                        </p>
                     </div>
-                    <div className='flex items-center gap-5 text-xl font-bold'>
-                        <img className="w-10" src="step3.svg" alt="" />
-                        <p >Step 3 : Special Offer</p>
+                    <div className="flex sm:flex-col items-center sm:gap-2 sm:py-6  lg:gap-5 sm:text-xs lg:text-xl font-bold">
+                        <img
+                            className="sm:w-5  lg:w-10"
+                            src="step3.svg"
+                            alt=""
+                        />
+                        <p>
+                            <span className="sm:hidden">Step 3 : </span>Special
+                            Offer
+                        </p>
                     </div>
-                    <div className='flex items-center gap-5 text-xl'>
-                        <img className="w-10" src="step4.svg" alt="" />
-                        <p >Step 4 : Confirmation</p>
+                    <div className="flex sm:flex-col items-center sm:gap-2 sm:py-6  lg:gap-5 sm:text-xs lg:text-xl">
+                        <img
+                            className="sm:w-5  lg:w-10"
+                            src="step4.svg"
+                            alt=""
+                        />
+                        <p>
+                            <span className="sm:hidden">Step 4 : </span>
+                            Confirmation
+                        </p>
                     </div>
                 </div>
             </div>
